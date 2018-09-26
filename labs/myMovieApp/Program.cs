9131677ca9ff778a62e76386be4065209a3cf3ac
Han Wang <Han.Wang@ey.com>
2018-09-26 14:54:36 -0400

    omnisharp

---
+++ labs/myMovieApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myMovieApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
